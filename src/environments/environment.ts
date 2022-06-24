9a05aae7ddf9421a8e749ed118f77aff2e8805b7
Alma Ibarra <utm20040090@utma.edu.mx>
2022-06-24 12:58:38 -0500

    Cambio para organizar mejor la URL de la API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// ----- aqui se ponen las constantes que utilizamos en todo el proyecto :D -----
+
 export const environment = {
-  production: false
+  production: false , 
+  urlNewsApi: "https://newsapi.org"
 };
 
 /*
